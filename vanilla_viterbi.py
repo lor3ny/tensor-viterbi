7a312cffa29f0c3f78a60d5af86a65e833211583
Lorenzo Piarulli <54499060+lor3ny@users.noreply.github.com>
2026-01-26 15:35:34 +0100

    Add initial implementation of vanilla Viterbi algorithm

---
+++ vanilla_viterbi.py
@@ -0,0 +1,29 @@
+
+
+
+def init():
+  # code here
+
+def inductive():
+  # code here
+
+def backtracking_termination():
+  # code here
+
+def main():
+  states = ['H','S','M']
+  obs = ['R','G', 'B','V']
+  input_obs ['R', 'R', 'G', 'B', 'R', 'V']
+
+  pi = [1,1,1]
+  
+  A = [0.1,0.2,0.7,
+       0.4,0.5,0.1,
+       0.2,0.2,0.6]
+
+  B = [0.1,0.1,0.3,0.5,
+       0.25,0.25,0.25,0.25,
+       0.3,0.1,0.3,0.3]
+
+if __name__ == "__main__":
+  main()
