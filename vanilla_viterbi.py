60ecc215d13a172a1c4ce2d806f6c59d769eab0f
Lorenzo Piarulli <54499060+lor3ny@users.noreply.github.com>
2026-01-29 15:31:00 +0100

    starting viterbi with Distance

--- vanilla_viterbi.py
+++ vanilla_viterbi.py
@@ -4,8 +4,35 @@
 def init():
   # code here
 
-def inductive():
+def inductive(states, input_obs, A, B, t_0):
   # code here
+
+  t_vals(len(states), len(input_obs))
+  t_vals[0] = t_0
+
+  for t in range(1, len(input_obs)):
+
+    ob = input_obs(pos)
+    max_vt = 0
+
+
+    # questa si può scrivere come grossa computazione matriciale
+    for sj in states:
+      for si in states:
+        for d in D:
+          max_vtij = t_vals(si, t-d) * A(i,j) * P(i,j,d)
+
+          if max_vtij > max_vt:
+            max_vt = max_vtij
+    
+    max_vt = max_vt * B(j, ob)
+
+    t_val[t] = max_vt
+
+
+
+
+  for s in states
 
 def backtracking_termination():
   # code here
